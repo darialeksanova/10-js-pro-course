e33086f0af9a2548f73e2f1f01d4555ed9eb5a71
Daria Aleksanova <darishubko@gmail.com>
2021-09-10 20:54:43 +0200

    test 123

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -2,3 +2,5 @@ const { smartSearch, randomSearch } = require('lesson-32-package');
 
 console.log(smartSearch(['Smith', 'John'], 'John'))
 console.log(randomSearch([1,2,3,4,5,6,7,8]))
+
+console.log('aga da')
