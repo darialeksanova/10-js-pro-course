60987e2a08be083ff238a9306c67c8efd311ccc8
Daria Aleksanova <darishubko@gmail.com>
2021-09-10 16:08:16 +0200

    create package beta version

--- lessons/32/public-package/index.js
+++ lessons/32/public-package/index.js
@@ -1,6 +1,14 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
+const randomSearch = (array) => {
+  const randomItemIndex = Math.floor(Math.random() * 10);
+    if(randomItemIndex < array.length) {
+      return array[randomItemIndex];
+    }
+      return randomSearch(array);
+};
 
 module.exports = {
-    smartSearch,
+  smartSearch,
+  randomSearch,
 } 
