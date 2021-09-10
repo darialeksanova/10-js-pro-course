1ae5adee89e4104645095786c7ba458cf2314b19
Daria Aleksanova <darishubko@gmail.com>
2021-09-10 20:53:05 +0200

    test 123

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -1,5 +1,4 @@
-// import functions from your package
 const { smartSearch, randomSearch } = require('lesson-32-package');
 
-console.log(smartSearch(['Smith', 'John'], 'John'));
-console.log(randomSearch([1,2,3,4,5,6,7,8]));
+console.log(smartSearch(['Smith', 'John'], 'John'))
+console.log(randomSearch([1,2,3,4,5,6,7,8]))
