372c16de2e6d764bce997a2ee9513adb54bdf369
Daria Aleksanova <darishubko@gmail.com>
2021-09-29 09:19:39 +0200

    Card

---
+++ lessons/37/lesson37-app/src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Card.css';
+
+type Props = {
+  backgroundColor: string;
+  children: JSX.Element,
+};
+
+function Card({children, backgroundColor}: Props) {
+  return (
+    <div 
+      className='card' 
+      style={{backgroundColor: backgroundColor || 'none'}}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Card;
