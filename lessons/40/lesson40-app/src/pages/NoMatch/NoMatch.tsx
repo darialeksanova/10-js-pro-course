fcb19f0ba63bf1de898a6a9e8b768eab5a94ac84
Daria Aleksanova <darishubko@gmail.com>
2021-10-25 10:55:46 +0200

    add dark theme to NoMatch component

--- lessons/40/lesson40-app/src/pages/NoMatch/NoMatch.tsx
+++ lessons/40/lesson40-app/src/pages/NoMatch/NoMatch.tsx
@@ -1,17 +1,27 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import styles from './NoMatch.module.css';
+import classNames from 'classnames/bind';
+import { Themes } from 'types/Theme';
+import { ThemeContext } from 'ThemeContext';
 
 type Props = {
   setIsDataLoaded: () => void;
 }
 
+const cx = classNames.bind(styles);
+
 const NoMatch = ({ setIsDataLoaded }: Props) => {
+  const theme = useContext(ThemeContext);
+
   useEffect(() => {
     setIsDataLoaded();
   }, [setIsDataLoaded]);
 
   return (
-    <div className={styles.noMatch}>
+    <div className={cx({
+      noMatch: true,
+      dark: theme === Themes.dark,
+    })}>
       <h1 className={styles.error}>404</h1>
       <h2 className={styles.message}>Oops! Page not found :c</h2>
     </div>
